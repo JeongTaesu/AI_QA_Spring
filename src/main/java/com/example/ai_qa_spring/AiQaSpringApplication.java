e3b9223bd2987ca58183a0af1fe77e265b938a43
JeongTaesu <taesu08@naver.com>
2023-08-16 15:30:48 +0900

    Initial commit

---
+++ src/main/java/com/example/ai_qa_spring/AiQaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_qa_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiQaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiQaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ai_qa_spring/AiQaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_qa_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiQaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
